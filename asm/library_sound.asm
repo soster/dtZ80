74bc514f8b56c0c60da9f5569dd50925c44a870d
Stefan Ostermann <stefan@ostermail.de>
2021-02-18 21:40:34 +0100

    First version with sound

---
+++ asm/library_sound.asm
@@ -0,0 +1,70 @@
+;Constants
+sevensegio equ $00 	    ;I/O port 7 segment A0-A7=0
+lcd_command equ $20	    ;LCD command I/O port, A5=1
+lcd_data equ $21        ;LCD data I/O port,A5=1,A0=1
+button	equ $40	        ;Button Input, dummy for now
+stackpointer equ $8fff  ;Address for the stack pointer
+
+;Startup command sequence for the lcd:
+;$3f: Function set: 8-bit interface, 2-line, small font
+;$0f: Display on, cursor on
+;$01: Clear display
+;$06: Entry mode: left to right, no shift
+;ff terminated.
+commands:
+    db $3f,$0f,$01,$06,$ff
+
+;7 segment numbers,0-9:
+numbers:
+    db $3f,$6,$5b,$4f,$66,$6d,$7d,$7,$7f,$6f,0
+
+message:
+    db "HELLO WORLD!",0
+
+;subroutines:
+
+;startup initializations
+startup:
+    ld sp,stackpointer
+    ret
+
+delay2:; 3 cascaded 8 bit loops to create some 100ms delay
+  push de
+  push bc
+    ld e,#4
+  loop1:
+    ld b,#40
+  loop2:
+    ld d,#ff
+  loop3:
+    dec	d
+    jp	nz,loop3
+    dec b
+    jp nz,loop2
+    dec e
+    jp nz,loop1
+  pop bc
+  pop de
+    ret
+
+segprint_num:;interpret a as a number 0-9 and translate it to the 7seg byte
+    push af     ;save af
+    push hl     ;save hl
+    push bc     ;save bc
+    ld hl,numbers
+    ld b,0
+    ld c,a
+    add hl,bc
+    ld a,(hl)
+    out (sevensegio),a
+    pop bc      ;restore bc
+    pop hl      ;restore hl
+    pop af      ;restore af
+    ret
+
+lcd_wait_loop:;wait until lcd is ready
+    in a,(lcd_command)      ;Read the status into A
+    ;trick to conditionally jump if bit 7 is true:
+    rlca                    ;Rotate A left, bit 7 moves into the carry flag
+    jr c,lcd_wait_loop      ;Loop back if the carry flag is set
+    ret
---
+++ asm/sound.asm
@@ -0,0 +1,64 @@
+;sound.z80
+;Count from 0-9 on the seven segment display
+;Depends on RAM and therefore uses call and ret
+
+;Constants
+register_port equ $d8
+data_port equ $d0
+
+
+org 0
+    call startup
+    ld hl,commands      ;Address of command list, $ff terminated
+
+mloop:
+    ld a,(hl)           ;Load character into A
+    and a               ;Test for end of string (A=0)
+    jr z,sound
+    ld a,b
+    call segprint_num
+    inc b
+    inc hl              ;Point to next character
+    call delay
+    jr mloop     ;Loop back for next character
+sound:
+  ld a, 7
+  out (register_port), a ;select the mixer register
+  ld a, 62
+  out (data_port),a ; enable channel A only
+  ld a, 8
+  out (register_port),a ; channel A volume
+  ld a, 15
+  out (data_port),a ; set it to maximum
+  ld a, 0
+  out (register_port),a; select channel A pitch
+reset:
+  ld b, 255
+loop:
+  ld a, b
+  out (data_port),a; set it
+  call pause
+  dec b
+  jp nz, loop
+  jp reset
+pause:
+  push bc
+  push de
+  push af
+
+  LD BC, $1500            ;Loads BC with hex 1000
+outer:
+  DEC BC                  ;Decrements BC
+  LD A, B                 ;Copies B into A
+  OR C                    ;Bitwise OR of C with A (now, A = B | C)
+  JP NZ, outer            ;Jumps back to Outer: label if A is not zero
+
+  pop af
+  pop de
+  pop bc
+  RET                     ;Return from call to this subroutine
+
+done:
+    halt
+
+INCLUDE 'library.asm'
