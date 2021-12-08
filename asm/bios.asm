89b16b331579a92c4fe3cb08b1634da804fc76c2
Stefan Ostermann <stefan@ostermail.de>
2021-12-08 20:32:56 +0100

    formatting

--- asm/bios.asm
+++ asm/bios.asm
@@ -16,42 +16,42 @@ RESET:
         ; Main code
         org $0100           ; main code starts at $0100 because of interrupt vector!
 MAIN:
-	ld sp,stackpointer
-	call SET_CTC         	; configure CTC
-	call SET_SIO         	; configure SIO
+        ld sp,stackpointer
+        call SET_CTC         	; configure CTC
+        call SET_SIO         	; configure SIO
 
-	call  A_RTS_OFF
+        call A_RTS_OFF
 
-	call LCD_PREPARE
-	ld hl,STARTUP_STR
-	call LCD_MESSAGE
+        call LCD_PREPARE
+        ld hl,STARTUP_STR
+        call LCD_MESSAGE
 
-	ld a,0             	 	; set high byte of interrupt vectors to point to page 0
-	ld i,a
+        ld a,0             	 	; set high byte of interrupt vectors to point to page 0
+        ld i,a
 
-	im 2               		; set int mode 2
-	ei                      ; enable interupt
+        im 2               		; set int mode 2
+        ei                      ; enable interupt
 
 
 ENDLESS_LOOP:
         ld d,$ff
         call DELAY
         call ENDLESS_LOOP
-	
+
 
 
 RX_CHA_AVAILABLE:
-		ex af,af'				 ;save registers
-		exx					 ;save registers
+        ex af,af'				 ;save registers
+        exx					 ;save registers
 
-		ld  a,(TEXT_COUNTER)
-		inc  a
-		ld  (TEXT_COUNTER),a
-        
+        ld a,(TEXT_COUNTER)
+        inc a
+        ld (TEXT_COUNTER),a
+
         in a,(SIO_DA)      ; read RX character into A
 
         ld hl,SCAN_LOOKUP     ; fetch scancode from lookup table
-        ld b,0                
+        ld b,0
         ld c,a
         adc hl,bc
         ld a,(HL)
@@ -63,35 +63,35 @@ RX_CHA_AVAILABLE:
         ;ld  hl,TEXT_BUFFER
         ;ld  (hl),a        
         ;call  LCD_MESSAGE
-      
-        out  (lcd_data),a
-        call  LCD_WAIT_SHORT
+
+        out (lcd_data),a
+        call LCD_WAIT_SHORT
 
         call TX_EMP          ; wait for outgoing char to be sent
         call RX_EMP            ; flush receive buffer
 
 
 
-END_AVAILABLE		
+END_AVAILABLE
         exx					;restore registers
-	ex af,af'					;restore registers
+        ex af,af'					;restore registers
 
         ei
         reti
 
 SPEC_RX_CONDITON:
-	jp $0000            ; if buffer overrun then restart the program
+        jp $0000            ; if buffer overrun then restart the program
 
 
 
-	include'dtz80-lib.inc'
-	include'sio-ctc-init.inc'
+        include'dtz80-lib.inc'
+        include'sio-ctc-init.inc'
 
 STARTUP_STR:
-	db 'dtZ80 Bios V 0.1 >',0
+        db 'dtZ80 Bios V 0.1 >',0
 TEXT_BUFFER:
-	db 0,0,0,0
+        db 0,0,0,0
 TEXT_COUNTER:
-	db 0
+        db 0
 RAMCELL:
-	db 0
+        db 0
